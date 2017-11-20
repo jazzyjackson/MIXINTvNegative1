a2949cd36db64789279d06891c4f54c64f86b58a
Colten Jackson <colten.jackson@annalect.com>
2017-11-20 12:11:59 -0500

    table updating works great

--- gui-blocks/table/class.js
+++ gui-blocks/table/class.js
@@ -16,10 +16,11 @@ class TableBlock extends TextareaBlock {
         })
     }
 
-    connectedCallback(){
-        this.initialized || this.dispatchEvent(new Event('init'))
-                         && this.dispatchEvent(new Event('ready'))
+    set hint(newVal){
+        this.child['footer'].style.display = newVal ? "block" : "none"
+        this.child['footer'].textContent = newVal
     }
+
 
     buildTable(arrayOfarrays){
         // destroy anything that's already there
@@ -31,10 +32,68 @@ class TableBlock extends TextareaBlock {
             for(var datum of row){
                 var td = document.createElement('td')
                 td.textContent = datum
-                tr.appendChild(td)
+                tr.appendChild(this.activateTableData(td))
             }
             this.child['table'].appendChild(tr)            
         }
+        this.hint = "Select a cell and hit return to edit."
+    }
+
+    activateTableData(td){
+        // setting an invisible property on the HTML object to retrieve if editing gets cancelled. td gets passed around
+        td.originalContent = td.textContent
+        td.setAttribute('tabIndex', 0) // allow td to be navigable by tabbing around
+        td.addEventListener("focus", event => { 
+            this.hint = "Hit Return to edit this cell."
+        })
+        td.addEventListener("keydown", event => { 
+            // this switches a TD into edit mode
+            if(event.shift || event.key != "Enter" || event.target != td) return null // exit if keydown wasn't enter, or if Shift+Enter was used
+            this.hint = "Hit Return to update the table, Escape to cancel the edit."
+            let staticCell = event.target
+            staticCell.removeAttribute("tabIndex") // so that if you tab out of the textarea the focus flow is to the next cell
+            let contentLength = staticCell.textContent.length
+            let contentHeight = staticCell.getClientRects()[0].height
+            staticCell.innerHTML = `<textarea tabIndex="0">${staticCell.textContent}</textarea>`
+            staticCell.firstChild.setSelectionRange(contentLength, contentLength)
+            staticCell.firstChild.focus()
+            staticCell.parentElement.style.minHeight = contentHeight + 'px'
+
+            staticCell.firstChild.addEventListener("blur", event => {
+                this.cancelEdit(staticCell)
+            })
+
+            staticCell.firstChild.addEventListener('keydown', event => {
+                if(event.shiftKey) return null // ignore events when shift is held down
+                switch(event.key){
+                    case "Escape":
+                        event.stopPropagation() // parent block is still listening for escape to focus menu, lets not bubble up
+                        this.cancelEdit(staticCell)
+                        staticCell.focus()
+                        break;
+                    case "Enter":
+                        this.updateTable(staticCell)
+                        break;
+                }
+            })
+        })
+        return td
+    }
+
+    cancelEdit(cellToCancel){
+        cellToCancel.textContent = cellToCancel.originalContent
+        cellToCancel.setAttribute("tabIndex", 0) // so that if you tab out of the textarea the focus flow is to the next cell
+    }
+
+    updateTable(cellToUpdate){
+        let colIndex = cellToUpdate.cellIndex
+        let rowIndex = cellToUpdate.parentElement.rowIndex
+        this.parsedData[rowIndex][colIndex] = cellToUpdate.firstChild.value
+        this.data = Papa.unparse(this.parsedData)
+        this.buildTable(this.parsedData)
+        this.child['table'].children[rowIndex].children[colIndex].focus()
+        this.hint = "Your changes aren't saved to disk yet, use +overwrite."
+
     }
 
     // add row and add column tables 
