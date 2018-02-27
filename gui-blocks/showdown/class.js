40a96e8606a1d909aa839f52f803d8e5e6f3af0c
Colten Jackson <colten.jackson@annalect.com>
2018-02-27 14:35:41 -0500

    showdown is a go

--- gui-blocks/showdown/class.js
+++ gui-blocks/showdown/class.js
@@ -1,14 +1,20 @@
 class ShowdownBlock extends TextareaBlock {
     constructor(props){
         super(props)
-        // this load will fire when the fetch for the file (in src attribute) completes
-        this.addEventListener('load', () => {
-            // this promise will resolve immediately if the script is already available
-            this.attachGlobalScript('/gui-blocks/showdown/assets/showdown.js').then(()=>{
-                this.converter = new showdown.Converter
-                this.child['showdown-container'].innerHTML = this.converter.makeHtml(this.data)
-                this.child['textarea'].style.display = 'none'
-            })
+        this.showdownOptions = {
+            literalMidWordUnderscores: true
+        }
+    }
+
+    static build(){
+        // Listening for load event, prototype TextareaBlock reacts to src attribute
+        Promise.all([
+            new Promise(resolve => this.addEventListener('load', resolve)),
+            this.attachGlobalScript(env.APP_HOME + '/gui-blocks/showdown/assets/showdown.js')
+        ]).then(()=>{
+            this.converter = new showdown.Converter(this.showdownOptions)
+            this.child['showdown-container'].innerHTML = this.converter.makeHtml(this.data)
+            this.child['textarea'].style.display = 'none'
         })
     }
 }
